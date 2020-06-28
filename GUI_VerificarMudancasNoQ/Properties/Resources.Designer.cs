a1544ff0bc313ad0fcde85e854e8bc4f4998c801
Vinícius Campos <vinibohm@hotmail.com>
2020-06-28 13:23:05 -0300

    Arquivo de projeto mudado para o .net core ser utilizado

--- GUI_VerificarMudancasNoQ/Properties/Resources.Designer.cs
+++ GUI_VerificarMudancasNoQ/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace GUI_VerificarMudancasNoQ.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace GUI_VerificarMudancasNoQ.Properties {
         ///   Retorna a instância de ResourceManager armazenada em cache usada por essa classe.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GUI_VerificarMudancasNoQ.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace GUI_VerificarMudancasNoQ.Properties {
         ///   pesquisas de recursos que usam essa classe de recurso de tipo de alta segurança.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace GUI_VerificarMudancasNoQ.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Icon semelhante a (Ícone).
         /// </summary>
-        internal static System.Drawing.Icon Icone_Q {
+        public static System.Drawing.Icon Icone_Q {
             get {
                 object obj = ResourceManager.GetObject("Icone_Q", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
@@ -73,7 +73,7 @@ namespace GUI_VerificarMudancasNoQ.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.IO.UnmanagedMemoryStream semelhante a System.IO.MemoryStream.
         /// </summary>
-        internal static System.IO.UnmanagedMemoryStream light {
+        public static System.IO.UnmanagedMemoryStream light {
             get {
                 return ResourceManager.GetStream("light", resourceCulture);
             }
